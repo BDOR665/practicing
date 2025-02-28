8411911d510b158b678ce2093e5f5ef7a5f11f5f
Yunyoung Jang <n0stress@naver.com>
2025-02-28 17:35:19 +0900

    remind3

--- src/main/java/com/javapractice/Main.java
+++ src/main/java/com/javapractice/Main.java
@@ -3,5 +3,7 @@ package com.javapractice;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Remind");
+        System.out.println("Remind");
+        System.out.println("Remind");
     }
 }
