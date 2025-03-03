d9a4c6c139fc4dc9ba5852550a011b5a624c2f79
Yunyoung Jang <n0stress@naver.com>
2025-03-04 00:18:22 +0900

    java_basic1_practice

--- src/main/java/com/javapractice/Main.java
+++ src/main/java/com/javapractice/Main.java
@@ -1,7 +1,18 @@
 package com.javapractice;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Remind");
+        int num1, num2;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("첫 번째 숫자를 입력해주세요 : ");
+        num1 = sc.nextInt();
+
+        System.out.println("두 번째 숫자를 입력해주세요 : ");
+        num2 = sc.nextInt();
+
+        System.out.println("입력받은 두 숫자 중에 더 큰 숫자는 : " + Math.max(num1,num2) + "입니다.");
     }
 }
